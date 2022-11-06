ec805c14013900e2d6d87b9da761133fbaac342e
gareth-39 <Gareth.A.Henvey@gmail.com>
2022-11-06 18:27:38 +0000

    Test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,117 @@
-console.log("Connected!")
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons){
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit"){
+                checkAnswer();
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        })
+    }
+
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if (event.key === "Enter"){
+            checkAnswer();
+        }
+    })
+
+    runGame("addition");
+
+})
+
+function runGame(gameType){
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
+    let num1 = Math.floor(Math.random() * 25) +1;
+    let num2 = Math.floor(Math.random() * 25) +1;
+
+    if (gameType === "addition"){
+        disaplayAdditionQuestion(num1, num2);    
+    } else if (gameType === "multiply"){
+        disaplayMultiplyQuestion(num1, num2);
+    } else if (gameType === "subtract"){
+        disaplaySubtractQuestion(num1, num2);
+    }else if (gameType === "division"){
+        disaplayDivisionQuestion(num1, num2);
+    }
+
+}
+
+function checkAnswer(){
+    
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if(isCorrect){
+        incrementScore();
+    } else {
+        incrementWrongAnswer();
+    }
+
+    runGame(calculatedAnswer[1]);
+}
+
+function calculateCorrectAnswer(){
+    
+    let operand1 = parseInt(document.getElementById("operand1").innerText)
+    let operand2 = parseInt(document.getElementById("operand2").innerText)
+    let operator = document.getElementById("operator").innerText
+
+    if (operator === "+"){
+        return [operand1 + operand2, "addition"]
+    } else if( operator === "x") {
+        return [operand1 * operand2, "multiply"]
+    } else if( operator === "-") {
+        return [operand1 - operand2, "subtract"]
+    } else if( operator === "÷") {
+        return [operand1 / operand2, "division"]
+    }
+}
+
+function incrementScore(){
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
+}
+
+function incrementWrongAnswer(){
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
+}
+
+function disaplayAdditionQuestion(operand1, operand2){
+
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
+
+}
+
+function disaplaySubtractQuestion(operand1, operand2){
+
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "-";
+
+}
+
+function disaplayMultiplyQuestion(operand1, operand2){
+
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
+
+}
+
+function disaplayDivisionQuestion(operand1, operand2){
+
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "÷";
+
+}
